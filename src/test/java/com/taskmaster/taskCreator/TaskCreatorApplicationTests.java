60982828ac440ae953580d49ae1f73c545580e2e
greta <greta.simonyan98@gmail.com>
2025-02-12 22:29:31 +0400

    initial commit

---
+++ src/test/java/com/taskmaster/taskCreator/TaskCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmaster.taskCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
